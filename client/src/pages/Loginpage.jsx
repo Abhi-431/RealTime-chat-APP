79413a956d9a44f0c771c1078e006307e139b261
Abhi 431 <abhishekbukoliya2204@gmail.com>
2026-05-27 01:52:38 +0530

    Login page complete .

--- client/src/pages/Loginpage.jsx
+++ client/src/pages/Loginpage.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 
 const Loginpage = () => {
+  const [currentState,SetCurrentState]=useState("Sign up")
+  const [fullName,setFullName]=useState("")
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+  const [bio,setBio]=useState("")
+  const [isDataSubmitted,setIsDataSubmitted]=useState("")
+  const onSubmitHandler=(event)=>{
+    event.preventDefault();
+    if(currentState==='Sign up' && !isDataSubmitted){
+      setIsDataSubmitted(true)
+      return;
+    }
+  }
   return (
-    <div>
-      Login page
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center
+    gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+     {/*Left Image*/}
+     <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
+     {/*Right Box*/}
+     <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+     <h2 className='font-medium text-2xl flex justify-between items-center'>
+      {currentState}
+      { isDataSubmitted && 
+      <img onClick={()=>setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />
+      }
+      </h2>
+
+
+     {currentState === "Sign up" && !isDataSubmitted &&(
+      <input type="text" placeholder='fullname' required className='p-2 border border-gray-500 
+     rounded-md focus:outline-none' />
+     )}
+
+     {!isDataSubmitted && (
+      <>
+      <input onChange={(e)=>setEmail(e.target.value)} value={email} 
+      type="email" placeholder='Email Address' required className='p-2 border border-gray-500
+      rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500' />
+      <input onChange={(e)=>setPassword(e.target.value)} value={password} 
+      type="password" placeholder='Password' required className='p-2 border
+       border-gray-500 rounded-md bg-transparent focus:outline-none focus:ring-2
+       focus:ring-indigo-500' />
+      </>
+     )}
+     {currentState === "Sign up" && isDataSubmitted &&(
+      <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
+      rows={4} className='p-2 border border-gray-500 rounded-md focus:outline-none
+      focus:ring-2 focus:ring-indigo-500'placeholder='Provide a short bio ..'required
+       ></textarea>
+     )}
+
+     <button type='submit' className='py-3 bg-gradient-to-r from-purple-400
+      to-violet-600 text-white rounded-md cursor-pointer'>
+      {currentState=== "Sign up" ? "Create Account":"Login now"}
+     </button>
+
+     <div className='flex items-center gap-2 text-sm text-gray-500'>
+      <input type='checkbox'/>
+      <p>Agree to terms of use and privasy policy.</p>
+     </div>
+     <div className='flex flex-col gap-2'>
+      {currentState === "Sign up"?(
+        <p className='text-sm text-gray-600'> Already have an account ?
+        <span className='font-mediul text-violet-500 cursor-pointer' onClick={()=>SetCurrentState("Login")}>Login Here</span></p>
+      ):(
+        <p className='text-sm text-gray-600'>
+          Create an account<span className='font-mediul text-violet-500 cursor-pointer ' onClick={()=>SetCurrentState("Sign up")} >Click here</span>
+        </p>
+      )}
+     </div>
+
+     </form>
     </div>
   )
 }
